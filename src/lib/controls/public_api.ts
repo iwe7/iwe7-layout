2feefc3695f771bb4d0d0bb2787fdc454f570be0
iwe7 <admin@meepo.com.cn>
2018-06-11 22:06:54 +0800

    导出loading

--- src/lib/controls/public_api.ts
+++ src/lib/controls/public_api.ts
@@ -3,3 +3,4 @@ export * from './iwe7-header.service';
 export * from './iwe7-mask.service';
 export * from './iwe7-menu-position';
 export * from './iwe7-menu.service';
+export * from './iwe7-loading.service';
